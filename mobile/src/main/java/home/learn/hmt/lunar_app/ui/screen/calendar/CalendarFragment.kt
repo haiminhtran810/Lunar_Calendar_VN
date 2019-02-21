9909dcbca52a2aec9badd4940b5dfa5104aaf764
haiminhtran810 <haiminhtran810@gmail.com>
2019-02-21 16:47:32 +0700

    Add Calendar

--- mobile/src/main/java/home/learn/hmt/lunar_app/ui/screen/calendar/CalendarFragment.kt
+++ mobile/src/main/java/home/learn/hmt/lunar_app/ui/screen/calendar/CalendarFragment.kt
@@ -6,10 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import home.learn.hmt.lunar_app.R
+import home.learn.hmt.lunar_app.model.CalendarDate
+import home.learn.hmt.lunar_app.widgets.listener.OnDateSelectedListener
 
-class CalendarFragment : Fragment() {
+class CalendarFragment : Fragment(), OnDateSelectedListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,4 +21,12 @@ class CalendarFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
 
+    override fun onDateSelected(date: CalendarDate) {
+        Toast.makeText(context, date.toString(), Toast.LENGTH_LONG).show()
+    }
+
+    companion object {
+        const val TAG = "CalendarFragment"
+        fun newInstance() = CalendarFragment()
+    }
 }
--- mobile/src/main/java/home/learn/hmt/lunar_app/widgets/CalendarDayView.kt
+++ mobile/src/main/java/home/learn/hmt/lunar_app/widgets/CalendarDayView.kt
@@ -2,22 +2,40 @@ package home.learn.hmt.lunar_app.widgets
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import home.learn.hmt.lunar_app.R
+import home.learn.hmt.lunar_app.model.CalendarDate
 import kotlinx.android.synthetic.main.view_calendar_day.view.*
 
 class CalendarDayView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null,
+    context: Context, calendarDate: CalendarDate, attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr) {
+
+    var mCalendarDate = calendarDate
     init {
-        View.inflate(getContext(), R.layout.view_calendar_day, this)
-        tv_calendar_day_text.text = "ABC"
+        LayoutInflater.from(context).inflate(R.layout.view_calendar_day, this)
+        calendarDate?.apply {
+            tv_calendar_day_text.text = mDay.toString()
+        }
     }
 
     fun setThisMothTextColor() {
+        tv_calendar_day_text.setTextColor(ContextCompat.getColor(context, R.color.blu_main))
+    }
+
+    fun setOtherMothTextColor() {
         tv_calendar_day_text.setTextColor(ContextCompat.getColor(context, R.color.greyishBrown))
+    }
+
+    fun setPurpleSolidOvalBackground() {
+        bg_day.setBackgroundResource(R.drawable.oval_purple_solid)
+    }
+
+    fun unsetPurpleSolidOvalBackground() {
+        bg_day.setBackgroundResource(R.drawable.oval_black_solid)
     }
 }
